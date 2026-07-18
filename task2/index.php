bda843c2cac2e1386e2e0c3f9bab6e05ea4f39c7
ganna55g <gannamohamed555g@gmail.com>
2026-07-19 00:21:39 +0300

    Update index.php

--- task2/index.php
+++ task2/index.php
@@ -1,4 +1,9 @@
-<?php session_start(); ?>
+<?php
+session_start();
+
+$old = $_SESSION["old"] ?? $_SESSION["user"] ?? [];
+$formId = $old["id"] ?? 105;
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,27 +15,34 @@
 <body>
     <h2>User Registration</h2>
 
-    <form action="process.php?id=105" method="POST" enctype="multipart/form-data">
+    <form action="process.php?id=<?php echo htmlspecialchars($formId); ?>" method="POST" enctype="multipart/form-data">
         <div class="box">
             <label for="Name">Name:</label>
-            <input type="text" name="name" id="Name">
+            <input type="text" name="name" id="Name" value="<?php echo htmlspecialchars($old["name"] ?? ""); ?>">
             <span style="color: red;">*<?php echo $_SESSION["errors"]["name"] ?? "" ?></span>
         </div>
          <br>
 
           <div class="box">
             <label for="Email">Email:</label>
-            <input type="email" name="email" id="Email">
+            <input type="email" name="email" id="Email" value="<?php echo htmlspecialchars($old["email"] ?? ""); ?>">
             <span style="color: red;">*<?php echo $_SESSION["errors"]["email"] ?? "" ?></span>
+        </div>
+        <br>
+
+        <div class="box">
+            <label for="Password">Password:</label>
+            <input type="password" name="password" id="Password">
+            <span style="color: red;">*<?php echo $_SESSION["errors"]["password"] ?? "" ?></span>
         </div>
         <br>
 
           <div class="box">
             <label for="lang">Language:</label>
              <select name="lang" id="lang">
-                <option value="" <?php echo (( $_SESSION["old"]["lang"] ?? "")==="")?"selected":""  ?> hidden></option>
-                <option value="ar">Arabic</option>
-                <option value="en">English</option>
+                <option value="" <?php echo (( $old["lang"] ?? "")==="")?"selected":""  ?> hidden></option>
+                <option value="ar" <?php echo (($old["lang"] ?? "")==="ar")?"selected":""; ?>>Arabic</option>
+                <option value="en" <?php echo (($old["lang"] ?? "")==="en")?"selected":""; ?>>English</option>
             </select>
             <span style="color: red;">*<?php echo $_SESSION["errors"]["lang"] ?? "" ?></span>
         </div>
@@ -47,7 +59,7 @@
         <button>submit</button>
 
     </form>
-    
+
 
 
 
